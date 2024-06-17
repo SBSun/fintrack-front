cceb246158f64702214176d3ea727bfa65bdbf5d
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-06-17 13:29:24 +0900

    fix: 로그인 여부에 따른 경로 설정 (#7)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CookiesProvider } from 'react-cookie';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
+  </React.StrictMode>
+);
