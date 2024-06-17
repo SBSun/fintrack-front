1fca9f740e5b787dd7dfa2488eb8210b37b358da
ByungSun Song <46879319+SBSun@users.noreply.github.com>
2024-06-17 13:04:15 +0900

    fix: 로그인 여부에 따른 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './component/LoginPage';
 import MainPage from './component/MainPage';
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    // 서버로 인증 상태 확인 요청
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get('https://fintrack.site/auth/status', {
+          withCredentials: true,
+        });
+        setIsAuthenticated(response.data.data.isAuthenticated);
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<LoginPage />}></Route>
-          <Route path='/' element={<MainPage />}></Route>
+          <Route
+            path='/login'
+            element={isAuthenticated ? <Navigate to='/' /> : <LoginPage />}
+          />
+          <Route
+            path='/'
+            element={isAuthenticated ? <MainPage /> : <Navigate to='/login' />}
+          />
         </Routes>
       </BrowserRouter>
     </>
